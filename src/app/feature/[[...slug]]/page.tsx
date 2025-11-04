eba38678e6ccb029e0e28b0819bd30280a4523f8
mohammadmohammadi785-crypto <alisafamohammadi785@gmail.com>
2025-11-04 14:27:43 +0430

    Changes added

---
+++ src/app/feature/[[...slug]]/page.tsx
@@ -0,0 +1,29 @@
+import React from "react";
+
+async function page({ params }: { params: Promise<{ slug: string }> }) {
+  const { slug } = await params;
+  if (slug?.length === 2) {
+    return (
+      <div>
+        <h1 className="text-3xl text-center text-red-500">
+          this is a feature of {slug[0]} and concept of {slug[1]}
+        </h1>
+      </div>
+    );
+  } else if (slug?.length === 1) {
+    return (
+      <div>
+        <h1 className="text-3xl text-center text-red-500">
+          this is the feature {slug[0]}
+        </h1>
+      </div>
+    );
+  }
+  return (
+    <div>
+      <h1 className="text-5xl text-center">this is a feature page</h1>
+    </div>
+  );
+}
+
+export default page;
--- src/app/layout.tsx
+++ src/app/layout.tsx
@@ -1,16 +1,16 @@
 import type { Metadata } from "next";
-// import { Inter, Geist_Mono } from "next/font/google";
+import { Inter, Geist_Mono } from "next/font/google";
 import "./globals.css";
 
-// const inter = Inter({
-//   variable: "--font-geist-sans",
-//   subsets: ["latin"],
-// });
+const inter = Inter({
+  variable: "--font-geist-sans",
+  subsets: ["latin"],
+});
 
-// const geistMono = Geist_Mono({
-//   variable: "--font-geist-mono",
-//   subsets: ["latin"],
-// });
+const geistMono = Geist_Mono({
+  variable: "--font-geist-mono",
+  subsets: ["latin"],
+});
 
 export const metadata: Metadata = {
   title: "Create Next 16",
@@ -24,7 +24,9 @@ export default function RootLayout({
 }>) {
   return (
     <html lang="en">
-      <body className={`antialiased`}>{children}</body>
+      <body className={`${inter.variable} ${geistMono.variable} antialiased`}>
+        {children}
+      </body>
     </html>
   );
 }

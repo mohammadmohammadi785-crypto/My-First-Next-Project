bb2e17ed1024a768c5511dff1251416c6a1b3243
mohammadmohammadi785-crypto <alisafamohammadi785@gmail.com>
2025-11-06 03:54:37 +0430

    Changes added

--- src/app/posts/page.tsx
+++ src/app/posts/page.tsx
@@ -2,18 +2,18 @@ import Link from "next/link";
 
 function page() {
   return (
-    <div className="p-2">
-      <h1 className="mb-1">All post</h1>
-      <Link className="ml-2" href="posts/1">
+    <div className="p-2 flex justify-center items-center flex-col">
+      <h1 className="mb-1 text-5xl">All post</h1>
+      <Link className="ml-2 text-3xl" href="posts/1">
         post 1
       </Link>
-      <Link className="ml-2" href="posts/2">
+      <Link className="ml-2 text-3xl" href="posts/2">
         post 2
       </Link>
-      <Link className="ml-2" href="posts/3">
+      <Link className="ml-2 text-3xl" href="posts/3">
         post 3
       </Link>
-      <Link className="ml-2" href="posts/4">
+      <Link className="ml-2 text-3xl" href="posts/4">
         post 4
       </Link>
     </div>

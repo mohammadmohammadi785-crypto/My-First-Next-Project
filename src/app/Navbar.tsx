0884a3801b8741131a01e1d56a9bb94aa34b214b
mohammadmohammadi785-crypto <alisafamohammadi785@gmail.com>
2025-11-16 22:39:38 +0430

    Changes added

---
+++ src/app/Navbar.tsx
@@ -0,0 +1,31 @@
+import React from "react";
+
+export default function Navbar() {
+  const navLinks: { id: number; name: string; href: string }[] = [
+    {
+      id: 1,
+      name: "Home",
+      href: "/",
+    },
+    {
+      id: 2,
+      name: "feature",
+      href: "/feature",
+    },
+    {
+      id: 3,
+      name: "About",
+      href: "/about",
+    },
+    {
+      id: 4,
+      name: "Contact",
+      href: "/contact",
+    },
+  ];
+  return (
+    <div>
+    
+    </div>
+  );
+}
